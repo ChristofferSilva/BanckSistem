5f74159d2dad49ec811c85841c49d8fe658596e9
Christoffer Bueno <christoffersilvadev123@gmail.com>
2026-06-20 16:59:12 -0300

    segundo commit

---
+++ src/main/java/projeto/BanckStatementProcessor.java
@@ -0,0 +1,10 @@
+package projeto;
+
+import java.util.List;
+
+public class BanckStatementProcessor {
+    private  final List<String> transactions;
+    public BanckStatementProcessor(List<String> transactions) {
+        this.transactions = transactions;
+    }
+}
